59e53fa6eb59c51ce062ce2f8ff646da8b304654
Attila Orosz <attila.orosz@mail.com>
2017-02-17 11:29:18 +0100

    Re-licensed to MIT license. De-licensed documentation

--- src/install-minimal.sh
+++ src/install-minimal.sh
@@ -1,25 +1,26 @@
 #!/bin/bash
 
-# Lazy Admin is a simple bash based pseudo UI to easily store
-# and execute bash commands and user functions
-#   Copyright (C) 2017  Attila Orosz
+# Lazy Admin
 #
-# This file is part of Lazy Admin
+# Copyright © 2017, Attila Orosz
 #
-# Lazy Admin is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
 #
-# You should have received a copy of the GNU Affero General Public License
-# along with Lazy Admin. If not, see <http://www.gnu.org/licenses/>.
-#
-# Contact: attila.orosz@mail.com
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
 
 installtarball=./files-v1.0-min.tar.gz
 
@@ -219,7 +220,7 @@ if [[ -d "$HOME/.LazyAdmin_Minimal/" ]]; then
    installdir="$HOME/.LazyAdmin_Minimal"
 
    tar xvzC "$installdir" -f $installtarball "core"
-   
+
 
 
 else
